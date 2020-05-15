8f52f66494e82709c6128ccc078cf704fe35a002
Irene Scott <irenejinscott@gmail.com>
2020-05-15 18:19:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+class Dog 
+
+@@all = []
+
+
+def initialize(puppy)
+  @puppy = puppy
+  self.save
+end
+
+def self.all
+  @@all
+end
+
+def self.print_all
+  @@all.each do |puppy_name|
+    puts puppy_name.puppy
+ end 
+end
+  
+  def save
+    @@all << self
+  end
+  
+end
+
+
+
+
+
+
